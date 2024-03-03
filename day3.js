8a1c5b95bc6bf30446df63c76951d340b391dc93
Tharsha6 <tharsha9956@gmail.com>
2024-03-03 20:12:28 +0530

    study

---
+++ day3.js
@@ -0,0 +1,13 @@
+const person=
+{
+    "fname":"Tharsha",
+    "Lname":"Loges",
+    "age":25
+
+};
+
+console.log(person.fname);
+
+let no = 9.83
+console.log(no.toFixed(3))
+console.log(no.toPrecision())
---
+++ firstday.js
@@ -0,0 +1,68 @@
+const name1="Tharsha"
+let name2="Loges"
+var name3="Hello"
+
+console.log(name1)
+
+let var1 = "1"
+
+//console.log(typeof(var3)) // print the datatype of the variable
+
+//console.log(var1 * 2);//JS convert the string to a number when there is a arithmetic operation on a string 
+                      //which contains a numerical value
+
+//console.log(var1 + 2);//If one or both operands are string the '+' operator performs addition
+
+// block - const, let
+// function - let, const and var
+
+//console.log(1 == "1") // here it checks only the value ,if value is ok print true
+//console.log(1 === "1") //here it checks value and also the data type
+
+
+// console.log("Hello my name is " + name1); // my name is Tharsha
+
+// console.log(`Hello my name is ${name1}`); // this is the standard way
+// console.log(name1.indexOf("r"));
+// console.log(name1[5]);
+
+//let string = "         Hello I am doing well!!         "
+
+// console.log(string.length); 
+// console.log(string.lastIndexOf("e"));
+// console.log(string.substring(4, 8)); // 8-1 (n - 1)
+
+// console.log(string.charAt(2));
+// console.log(string.trim());
+
+// const url = "www.google.com"
+// console.log(url.startsWith("www"));
+
+// console.log(url.replace("google.com", "youtube.com"));
+// console.log(string.includes("etewtaking"));
+
+// const string1 = "This is our js journey"
+// const newString = new String(string1)
+
+// console.log(newString.split(" "));
+
+
+// let phoneNum = 70452630
+// const num = new Number(234567)
+// // console.log(typeof(num.toString())); // change the datatype int to string
+
+
+// const decimal = 123.984234
+// console.log(decimal.toFixed(2));
+// console.log(decimal.toPrecision(4));
+
+
+console.log(Math.random() * 10);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+console.log(Math.ceil(2.0001))
+
